a83e16e1c72fde72f62f7a376d1a1ab3fe0ee97e
Nghia Tran <vinhnghiatran@gmail.com>
2022-11-25 10:30:21 +0700

    Remove cocoapod. Update deps

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SDWebImageAVIFCoder",
     platforms: [
-        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_13), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -18,7 +18,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
-        .package(url: "https://github.com/SDWebImage/libavif-Xcode.git", from: "0.9.1")
+        .package(url: "https://github.com/SDWebImage/libavif-Xcode.git", from: "0.10.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
